2b73f0c7072b56b1d03b40bc8f188eaecb414215
Amer Varajic <varajicamer@gmail.com>
2023-03-03 17:37:47 +0100

    Adding the "Cart" Button Component

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import CartIcon from "../Cart/CartIcon";
+import calsses from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={calsses.button}>
+      <span className={calsses.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={calsses.badge}>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
