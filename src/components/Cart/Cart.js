986775b4f826a354475d9fee0f1ea98aeb5db7f1
Amer Varajic <varajicamer@gmail.com>
2023-03-09 15:21:07 +0100

    Outputting Cart Items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,33 @@
+import React, { useContext } from "react";
+
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  // importujemo useContext, te kreiramo cartCtx, koja ce nam zamijeniti prethodne fiktivne podatke
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        </li>
       ))}
     </ul>
   );
@@ -16,14 +38,15 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
         {/* onClick koristimo kako bi upotrjebili hideCartHandler funkciju */}
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
+        {/* hasItems funkcija ima za zadatak da u koliko u Order-u ima bar jedan item, funkcija ovog gumba ce raditi */}
       </div>
     </Modal>
   );
