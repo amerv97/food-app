1b509319a82d44d5f5ac6b266f2b09737725d77a
Amer Varajic <varajicamer@gmail.com>
2023-03-11 16:16:59 +0100

    Making Items Removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -33,6 +33,29 @@ const cartRedrucer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+      // Utvrdjivanje da li imamo Item u Cart-u
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    // Racunamo iznos koji trebamo platiti
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      // ako je vise od jednog itema, zelimo samo da smanjimo amount
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
   return defaultCartState;
 };
 // state = is last snapshoot
